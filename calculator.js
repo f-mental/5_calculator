ca361073644dafed5e923f3776439c343c11813d
fmentalwinds <ffoolmental@gmail.com>
2022-11-10 21:05:38 +0800

    Add logic to enable consecutive calculation
    Can now reuse the value last returned by performOperation all over and over again

--- calculator.js
+++ calculator.js
@@ -6,6 +6,8 @@ let firstNumber;
 let secondNumber;
 let currentOperation;
 let result;
+let beginCalculation = true;
+let beginFromFirst = false;
 
 function performOperation (currentOperation, firstNumber, secondNumber) {
     firstNumber = parseInt(firstNumber);
@@ -27,28 +29,46 @@ function performOperation (currentOperation, firstNumber, secondNumber) {
 
 keys.forEach(key => {
     key.addEventListener('click', ev => {
-        if (bottomScreen.innerText === '0') {
+        if (beginCalculation) {
+            beginCalculation = false;
             bottomScreen.innerText = '';
             bottomScreen.innerText += ev.target.value;
         } else {
-            if (firstNumber) {
+            if (firstNumber || beginFromFirst) {
+                beginFromFirst = false;
                 if (secondNumber) {
                     if (ev.target.value === '=') {
                         if (!result) {
                             secondNumber = bottomScreen.innerText;
-                        }
+                        }   
                         topScreen.innerText = `${firstNumber}${currentOperation}${secondNumber}=`
                         result = performOperation(currentOperation, firstNumber, secondNumber);
                         bottomScreen.innerText = result;
                         firstNumber = result;
                     } else {
+                        if (result && firstNumber && secondNumber) {
+                            // reset input when all both numbers and result exist
+                            if (/d/.test(ev.target.value)) {
+                                result = 0;
+                                firstNumber = 0;
+                                secondNumber = 0;
+                                bottomScreen.innerText = '';
+                                topScreen.innerText = '';
+                            } else if (operations.test(ev.target.value)) {
+                                currentOperation = ev.target.value;
+                                firstNumber = result;
+                                topScreen.innerText = `${firstNumber}${currentOperation}`
+                                secondNumber = 0;
+                                beginFromFirst = true;
+                            }
+                        }
                         bottomScreen.innerText += ev.target.value;
                     }
                 } else {
                     bottomScreen.innerText = '';
                     bottomScreen.innerText += ev.target.value;
                     secondNumber = bottomScreen.innerText;
-                }
+                } 
             } else {
                 if (operations.test(ev.target.value)) {
                     currentOperation = ev.target.value;
