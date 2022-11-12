2b3c5985b2c94c51c2f17de68efed1cc3521c4b2
fmentalwinds <ffoolmental@gmail.com>
2022-11-12 20:21:35 +0800

    Make reset and del buttons to work
    Also added special rules how zero digit input will work. Zero can reset calculation or zero must not be duplicated in bottomScreen if bottomScreen only display '0'

--- calculator.js
+++ calculator.js
@@ -28,40 +28,62 @@ function performOperation (currentOperation, firstNumber, secondNumber) {
     }
 }
 
+function resetCalculation () {
+    firstNumber = null;
+    secondNumber = null;
+    result = null;
+    bottomScreen.innerText = '';
+    topScreen.innerText = '';
+}
+
 // not sure what is happening with floats, check it thoroughly
+// float -> https://www.w3schools.com/js/js_numbers.asp
+// try to remove beginfromFirst flag
+// check for any bugs
 
 keys.forEach(key => {
     key.addEventListener('click', ev => {
         let buttonPressed = ev.target.value;
 
         // if digit is pressed
-        if (/\d/.test(buttonPressed)) {
+        if (/[1-9]/.test(buttonPressed)) {
             //if the calculator program is just started, beginCalculation flag is set to true
             if (beginCalculation){
                 beginCalculation = false;
                 bottomScreen.innerText = '';
                 bottomScreen.innerText += buttonPressed;
-            } else if (result && firstNumber && secondNumber) {
+            } else if (result || (result === 0)) {
                 // when a different number is pressed when there is an ongoing calculation
-                console.log('here');
-                result = null;
-                firstNumber = null;
-                secondNumber = null;
-                bottomScreen.innerText = '';
-                topScreen.innerText = '';
+                resetCalculation();
                 bottomScreen.innerText += buttonPressed;
             } else if (firstNumber && (!beginFromFirst)) {
                 bottomScreen.innerText = '';
                 bottomScreen.innerText += buttonPressed;
                 // beginFromFirst flag is set to true to indicate that the secondNumber is about to 
                 // be generated
-                beginFromFirst = true; 
-            } else if (beginFromFirst) {
-                bottomScreen.innerText += buttonPressed; 
-            } else {
+                beginFromFirst = true;
+            } else if ((bottomScreen.innerText === '0')) {
+                bottomScreen.innerText = '';
+                bottomScreen.innerText += buttonPressed;
+            }else {
                 bottomScreen.innerText += buttonPressed;
             }
 
+        }
+
+        // if zero is pressed
+        if (buttonPressed === '0') {
+            if (result || (result === 0)) {
+                resetCalculation();
+                bottomScreen.innerText += buttonPressed;
+                beginCalculation = true;
+            }        
+            else if (firstNumber) {
+                bottomScreen.innerText = '';
+                bottomScreen.innerText += buttonPressed;
+            } else if(!(bottomScreen.innerText === '0')) {
+                bottomScreen.innerText += buttonPressed;
+            }
         }
 
         // if decimal is pressed
@@ -98,16 +120,32 @@ keys.forEach(key => {
                 result = performOperation(currentOperation, firstNumber, secondNumber);
                 bottomScreen.innerText = result;
                 firstNumber = result;
+                isDecimalActive = true;
             }
         }
 
-
         // if reset is pressed
-
+        if (buttonPressed === 'reset') {
+            resetCalculation();
+            bottomScreen.innerText += '0';
+            beginCalculation = true;
+        }
 
         // if del is pressed
-
-
+        if (buttonPressed === 'del') {
+            // if (result) {
+            //     let lastBottomDisplay = bottomScreen.innerText;
+            //     resetCalculation();
+            //     bottomScreen.innerText = lastBottomDisplay
+            //     beginCalculation = true;
+            // } else {}
+                let bottomTextLength = bottomScreen.innerText.length;
+                if (bottomTextLength > 1) {
+                    bottomScreen.innerText = bottomScreen.innerText.slice(0, bottomTextLength-1)
+                } else {
+                    bottomScreen.innerText = '0';
+                }
+        }
 
     })
 })
